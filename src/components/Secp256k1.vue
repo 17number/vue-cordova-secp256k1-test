c80bebb18e765d45775be0660f6874a3c02b1650
r17n <4716989+17number@users.noreply.github.com>
2019-11-28 12:27:24 +0900

    chore: time measurement

--- src/components/Secp256k1.vue
+++ src/components/Secp256k1.vue
@@ -20,7 +20,17 @@
       <h4>valid(secp256k1-node)</h4>
       <p>{{ verifidSecp256k1Node }}</p>
     </div>
-
+    <div>
+      <h3>Times</h3>
+      <h4>generateKeyPair</h4>
+      <p>{{ times.generateKeyPair }}</p>
+      <h4>initSecp256k1</h4>
+      <p>{{ times.initSecp256k1 }}</p>
+      <h4>signAndVerifyBtcTs</h4>
+      <p>{{ times.signAndVerifyBtcTs }}</p>
+      <h4>signAndVerifySecp256k1Node</h4>
+      <p>{{ times.signAndVerifySecp256k1Node }}</p>
+    </div>
   </div>
 </template>
 
@@ -44,14 +54,26 @@ export default class Secp256k1 extends Vue {
   verifidBtcTs = false;
   signSecp256k1Node = '';
   verifidSecp256k1Node = false;
+  times = {
+    generateKeyPair: -1,
+    initSecp256k1: -1,
+    signAndVerifyBtcTs: -1,
+    signAndVerifySecp256k1Node: -1,
+  };
   generateKeyPair() {
+    const s = new Date();
     do {
       this.privKey = randomBytes(32);
     } while(!secp256k1.privateKeyVerify(this.privKey));
     this.pubKey = secp256k1.publicKeyCreate(this.privKey);
+    const e = new Date();
+    this.times.generateKeyPair = e.getTime() - s.getTime();
   }
   async initSecp256k1() {
+    const s = new Date();
     this.secp256k1Entity = await instantiateSecp256k1();
+    const e = new Date();
+    this.times.initSecp256k1 = e.getTime() - s.getTime();
   }
   hashMessage() {
     this.msgHashed = Buffer.from(cryptoJs.SHA256(this.msg).toString(), 'hex');
@@ -59,14 +81,20 @@ export default class Secp256k1 extends Vue {
     this.signAndVerifySecp256k1Node();
   }
   signAndVerifyBtcTs() {
+    const s = new Date();
     this.signBtcTs = this.secp256k1Entity.signMessageHashCompact(this.privKey, this.msgHashed);
     this.signBtcTsStr = Buffer.from(this.signBtcTs).toString('hex');
     this.verifidBtcTs = this.secp256k1Entity.verifySignatureCompact(this.signBtcTs, this.pubKey, this.msgHashed);
+    const e = new Date();
+    this.times.signAndVerifyBtcTs = e.getTime() - s.getTime();
   }
   signAndVerifySecp256k1Node() {
+    const s = new Date();
     const sigObj = secp256k1.sign(this.msgHashed, this.privKey);
     this.signSecp256k1Node = sigObj.signature.toString('hex');
     this.verifidSecp256k1Node = secp256k1.verify(this.msgHashed, sigObj.signature, this.pubKey);
+    const e = new Date();
+    this.times.signAndVerifySecp256k1Node = e.getTime() - s.getTime();
   }
   created() {
     this.generateKeyPair();
